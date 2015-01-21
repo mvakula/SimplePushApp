04c1dfc85577e5cbc9eca86c2907ca4768f55fc7
Mika Väisänen <mika.vaisanen@aalto.fi>
2015-01-21 20:12:44 +0200

    Moved PushPlugin code to client and made a method to store token to DB. However, it prints an error to xcode's console and the code doesn't work.

--- myapp.js
+++ myapp.js
@@ -1,4 +1,58 @@
 if (Meteor.isClient) {
+
+  Meteor.startup(function() {
+    // Cordova pushplugin
+    var pushNotification = window.plugins.pushNotification;
+
+
+    function tokenSuccessHandler(result) {
+      console.log('token success result ' + result);
+      window.localStorage.setItem('deviceToken', result);
+      // API call to store token on your DB
+      Meteor.call('savePushTokens', result)
+      console.log(Tokens.find().fetch());
+    }
+
+    function successHandler(result) {
+      console.log('success result ' + result);
+      // API call to store token on your DB
+    }
+
+    function errorHandler(error) {
+      console.log('error result ' + error);
+    }
+
+    function onNotificationAPN(e) {
+      console.log('Inside the onNotificationAPN callback!');
+      if ( e.alert ) {
+        navigator.notification.alert(e.alert);
+      }
+
+      if ( e.sound ) {
+        var snd = new Media(e.sound);
+        snd.play();
+      }
+
+      if ( e.badge ) {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.badge);
+      }
+    }
+
+    window.onNotificationAPN = onNotificationAPN;
+
+    pushNotification.register(
+      tokenSuccessHandler,
+      errorHandler, {
+        'badge':'true',
+        'sound':'true',
+        'alert':'true',
+        'ecb':'onNotificationAPN'
+      }
+    );
+  });
+  
+
+  // Test content
   // counter starts at 0
   Session.setDefault("counter", 0);
 
@@ -14,63 +68,4 @@ if (Meteor.isClient) {
       Session.set("counter", Session.get("counter") + 1);
     }
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
-
-if (Meteor.isCordova) {
-Meteor.startup(function() {
-
-var pushNotification = window.plugins.pushNotification;
-
-
-function tokenSuccessHandler(result) {
-  console.log('token success result ' + result);
-  window.localStorage.setItem('deviceToken', result);
-  // API call to store token on your DB
-  Tokens.insert({token: result});
-  console.log(Tokens.find().fetch());
-}
-
-function successHandler(result) {
-  console.log('success result ' + result);
-  // API call to store token on your DB
-}
-
-function errorHandler(error) {
-  console.log('error result ' + error);
-}
-
-function onNotificationAPN(e) {
-  console.log('Inside the onNotificationAPN callback!');
-  if ( e.alert ) {
-    navigator.notification.alert(e.alert);
-  }
-
-  if ( e.sound ) {
-    var snd = new Media(e.sound);
-    snd.play();
-  }
-
-  if ( e.badge ) {
-    pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.badge);
-  }
-}
-
-window.onNotificationAPN = onNotificationAPN;
-
-pushNotification.register(
-  tokenSuccessHandler,
-  errorHandler, {
-    'badge':'true',
-    'sound':'true',
-    'alert':'true',
-    'ecb':'onNotificationAPN'
-  }
-);
-});
 }
--- server/apn.js
+++ server/apn.js
@@ -22,7 +22,7 @@ Meteor.methods({
         // expires 1 hour from now
         note.expiry = Math.floor(Date.now() / 1000) + 3600
         note.sound = "ping.aiff";
-        note.badge = 3;
+        note.badge = 1;
         note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
         note.payload = {'messageFrom': 'Caroline'};
 
