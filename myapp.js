ac9f082ccfd91149877114e8ff493fc7b9dd09f6
Mika Väisänen <mika.vaisanen@aalto.fi>
2015-01-21 11:44:32 +0200

    apn working but pushplugin is used directly

--- myapp.js
+++ myapp.js
@@ -19,6 +19,58 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-    Push.debug=true;
   });
+}
+
+if (Meteor.isCordova) {
+Meteor.startup(function() {
+
+var pushNotification = window.plugins.pushNotification;
+
+
+function tokenSuccessHandler(result) {
+  console.log('token success result ' + result);
+  window.localStorage.setItem('deviceToken', result);
+  // API call to store token on your DB
+  Tokens.insert({token: result});
+  console.log(Tokens.find().fetch());
+}
+
+function successHandler(result) {
+  console.log('success result ' + result);
+  // API call to store token on your DB
+}
+
+function errorHandler(error) {
+  console.log('error result ' + error);
+}
+
+function onNotificationAPN(e) {
+  console.log('Inside the onNotificationAPN callback!');
+  if ( e.alert ) {
+    navigator.notification.alert(e.alert);
+  }
+
+  if ( e.sound ) {
+    var snd = new Media(e.sound);
+    snd.play();
+  }
+
+  if ( e.badge ) {
+    pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.badge);
+  }
+}
+
+window.onNotificationAPN = onNotificationAPN;
+
+pushNotification.register(
+  tokenSuccessHandler,
+  errorHandler, {
+    'badge':'true',
+    'sound':'true',
+    'alert':'true',
+    'ecb':'onNotificationAPN'
+  }
+);
+});
 }
---
+++ server/apn.js
@@ -0,0 +1,35 @@
+var apn = Meteor.npmRequire("apn"),
+    path = Npm.require('path'),
+    apnOptions = Meteor.settings.apnOptions || {},
+    apnConnection
+
+// default apn connection options
+var appRootPath = "/Users/Mika/Devaus/meteor/myapp/"
+apnOptions = _.extend({
+  cert: path.join(appRootPath, "private", "apnDevCert.pem"),
+  key: path.join(appRootPath, "private", "apnDevKey.pem"),
+  passphrase: "devdev",
+}, apnOptions)
+apnConnection = new apn.Connection(apnOptions)
+
+Meteor.methods({
+    sendPush: function() {
+        var token = "077f2ea72eb6b2dfc381ce27f2eb12e2ee8ee68f7eeb90f7f2f10f1d99cd140e"
+        var apn = Meteor.npmRequire("apn")
+        var note = new apn.Notification()
+        var myDevice = new apn.Device(token);
+
+        // expires 1 hour from now
+        note.expiry = Math.floor(Date.now() / 1000) + 3600
+        note.sound = "ping.aiff";
+        note.badge = 3;
+        note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+        note.payload = {'messageFrom': 'Caroline'};
+
+        apnConnection.pushNotification(note, myDevice);
+
+        return {success: 'ok'}
+    }
+});
+
+Meteor.call('sendPush');
